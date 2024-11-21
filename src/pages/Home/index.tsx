5bb42168256a68c36a6e59bcd31505001d548e1e
nicolastmaia <nicolasterramaia@gmail.com>
2024-11-21 16:42:47 -0300

    feat: add loading icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,6 +11,7 @@ const Home = () => {
   const [serverIpAddress, setServerIpAddress] = useState<string>('')
   const [power, setPower] = useState<boolean>(false);
   const [isInitialLoading, setIsInitialLoading] = useState<boolean>(true);
+  const [isConnectionLoading, setIsConnectionLoading] = useState<boolean>(false)
 
   const navigate = useNavigate();
 
@@ -52,6 +53,7 @@ const Home = () => {
     }
 
     try {
+      setIsConnectionLoading(true)
       if (sRegion === -1) {
         selectedCountryIndex = Math.floor(Math.random() * allRegions.length)
         setSRegion(selectedCountryIndex);
@@ -62,99 +64,85 @@ const Home = () => {
       const selectedCountryCode = allRegions[selectedCountryIndex].code
 
       await startSilentPass(selectedCountryCode)
-      setPower(true);
+      setTimeout(() => {
+        setIsConnectionLoading(false)
+        setPower(true);
+      }, 5000)
       return
     } catch (error) {
       setPower(false);
     }
   };
 
-  return (
-    <div className="home">
-      <div className="stats-container">
-        <div>Online Miners: </div>
-        <div>Online Users: </div>
-      </div>
-
-      {isInitialLoading ? (
+  const renderButton = () => {
+    if (isConnectionLoading)
+      return (
         <>
+          <p className="connection">Connecting <span> Silent Pass </span></p >
+
           <button
             className="power"
           >
-            <img className="silent-pass-logo-loading" src="/assets/silent-pass-logo-grey.png" width={85} height={85} alt="" />
+            <img src="/assets/loading-ring.png" className="loading-spinning" width={85} height={85} alt="" />
           </button>
 
-          <p className="not-connected">Welcome to Silent Pass</p>
+          <p className="connected">Loading...</p>
         </>
-      ) : (
+      )
+
+    if (power)
+      return (
         <>
-          {
-            power ? (
-              <p className="connection" >
-                Your connection is <span>protected!</span>
-              </p >
-            ) : (
-              <p className="connection">Your connection is not protected!</p>
-            )}
+          <p className="connection">Your connection is <span>protected!</span></p >
 
           <button
             className="power"
             onClick={handleTogglePower}
           >
-            {power ? (
-              <img src="/assets/power.png" width={83} height={85} alt="" />
-            ) : (
-              <img src="/assets/not-power.png" width={83} height={85} alt="" />
-            )}
+            <img src="/assets/power.png" width={85} height={85} alt="" />
           </button>
 
-          {
-            power ? (
-              <p className="connected">Connected</p>
-            ) : (
-              <p className="not-connected">Not Connected</p>
-            )
-          }
+          <p className="connected">Connected</p>
+        </>
+      )
 
-          {
-            !power && (
-              <div>
-                <button
-                  className="auto-btn"
-                  onClick={() => {
-                    if (sRegion === -1)
-                      setSRegion(Math.floor(Math.random() * allRegions.length));
-                  }}
-                >
-                  {sRegion === -1 ? (
-                    <>
-                      <img src="/assets/auto.png" width={24} height={24} alt="" />
-                      Auto Select
-                    </>
-                  ) : (
-                    <>
-                      <ReactCountryFlag
-                        countryCode={allRegions[sRegion].code}
-                        svg
-                        aria-label="United States"
-                        style={{
-                          fontSize: "2em",
-                          lineHeight: "2em",
-                        }}
-                      />
-                      {allRegions[sRegion].country}
-                    </>
-                  )}
-                </button>
-                <p className="home-location">Selected Location</p>
-              </div>
-            )
-          }
+    return (
+      <>
+        <p className="connection">Your connection is not protected!</p>
 
-          {
-            power ? (
-              <>
-                <div>
+        <button
+          className="power"
+          onClick={handleTogglePower}
+        >
+          <img src="/assets/not-power.png" width={85} height={85} alt="" />
+        </button>
+
+        <p className="not-connected">Not Connected</p>
+      </>
+    )
+  }
+
+  const renderRegionSelector = () => {
+    if (isConnectionLoading) return
+
+    return <>
+      {
+        !power && (
+          <div>
+            <button
+              className="auto-btn"
+              onClick={() => {
+                if (sRegion === -1)
+                  setSRegion(Math.floor(Math.random() * allRegions.length));
+              }}
+            >
+              {sRegion === -1 ? (
+                <>
+                  <img src="/assets/auto.png" width={24} height={24} alt="" />
+                  Auto Select
+                </>
+              ) : (
+                <>
                   <ReactCountryFlag
                     countryCode={allRegions[sRegion].code}
                     svg
@@ -162,45 +150,77 @@ const Home = () => {
                     style={{
                       fontSize: "2em",
                       lineHeight: "2em",
-                      marginRight: ".5em",
                     }}
                   />
                   {allRegions[sRegion].country}
+                </>
+              )}
+            </button>
+            <p className="home-location">Selected Location</p>
+          </div>
+        )
+      }
 
-                </div>
+      {
+        power ? (
+          <div>
+            <ReactCountryFlag
+              countryCode={allRegions[sRegion].code}
+              svg
+              aria-label="United States"
+              style={{
+                fontSize: "2em",
+                lineHeight: "2em",
+                marginRight: ".5em",
+              }}
+            />
+            {allRegions[sRegion].country}
+          </div>
+        ) : (
+          <button className="region-btn" onClick={() => navigate("/regions")}>
+            <div>
+              <img src="/assets/global.png" width={22} height={22} alt="" />
+              <p>Select Region</p>
+            </div>
+            <img src="/assets/right.png" width={4} height={8} alt="" />
+          </button>
+        )
+      }
+    </>
+  }
 
-                <div style={{ display: "flex", flexDirection: "column", gap: '20px', backgroundColor: '#1B1B1D', borderRadius: '16px', padding: '20px', width: 300, fontSize: '14px' }}>
-                  <div style={{ display: "flex", flexDirection: "column", gap: '4px' }}>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                      <div>Socks 5 Address:</div>
-                      <div style={{ color: '#B1B1B2' }}>{serverIpAddress}</div>
-                    </div>
+  return (
+    <>
+      <div className="home">
+        <div className="stats-container">
+          <div>Online Miners: </div>
+          <div>Online Users: </div>
+        </div>
 
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: 'space-between' }}>
-                      <div>Port: </div>
-                      <div style={{ color: '#B1B1B2' }}>3002</div>
-                    </div>
-                  </div>
+        {isInitialLoading ? (
+          <>
+            <button
+              className="power"
+            >
+              <img className="loading-spinning" src="/assets/silent-pass-logo-grey.png" width={85} height={85} alt="" />
+            </button>
 
-                  <div style={{ display: "flex", flexDirection: "row", justifyContent: 'space-between' }}>
-                    <div>PAC URL:</div>
-                    <div>{"http://" + serverIpAddress + "/pac"}</div>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <button className="region-btn" onClick={() => navigate("/regions")}>
-                <div>
-                  <img src="/assets/global.png" width={22} height={22} alt="" />
-                  <p>Select Region</p>
-                </div>
-                <img src="/assets/right.png" width={4} height={8} alt="" />
-              </button>
-            )
-          }
-        </>
-      )}
-    </div >
+            <p className="not-connected">Welcome to Silent Pass</p>
+          </>
+        ) : (
+          <>
+            {renderButton()}
+            {renderRegionSelector()}
+          </>
+        )}
+
+      </div >
+      <div className="footer">
+        <div className="footer-content">
+          © 2024 CoNET.network. All rights reserved
+        </div>
+      </div>
+    </>
   );
 };
 

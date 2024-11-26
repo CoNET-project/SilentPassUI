949412ab4ba4de1406c2223e8bab5373214d78a8
nicolastmaia <nicolasterramaia@gmail.com>
2024-11-26 19:11:37 -0300

    fix: change blob color

--- src/components/BlobWrapper/index.tsx
+++ src/components/BlobWrapper/index.tsx
@@ -10,11 +10,11 @@ export default function BlobWrapper({ children }: BlobWrapperProps) {
   return (
     <div className="blob-wrapper">
       <Blob
-        color="#232e44"
+        color="#0d111a"
         style={{ width: "220%", opacity: .8, position: "absolute", top: "50%", left: 0, transform: "translate(-18%, -40%)" }}
       />
       <Blob
-        color="#0c111c"
+        color="#07080C"
         style={{ width: "180%", position: "absolute", top: "50%", left: 0, transform: "translate(-12%, -40%)" }}
       />
       {children}

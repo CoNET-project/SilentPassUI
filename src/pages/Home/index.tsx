a044b196321afc1592cbafa887de91186245b592
nicolastmaia <nicolasterramaia@gmail.com>
2025-02-17 20:22:21 -0300

    fix: hide passport info in home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -279,7 +279,7 @@ const Home = () => {
 
             <CopyProxyInfo />
 
-            <PassportInfo />
+            {/* <PassportInfo /> */}
 
             {!power && !isConnectionLoading &&
               <RegionSelector

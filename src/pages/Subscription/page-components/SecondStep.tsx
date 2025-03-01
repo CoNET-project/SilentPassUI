adf6f526328aae4919d9832f30baf2d9f080a2e7
nicolastmaia <nicolasterramaia@gmail.com>
2025-03-01 09:18:24 -0300

    style: show asset user is buying in purchase confirmation page

--- src/pages/Subscription/page-components/SecondStep.tsx
+++ src/pages/Subscription/page-components/SecondStep.tsx
@@ -4,6 +4,7 @@ import Separator from '../../../components/Separator';
 import { ReactComponent as UsdtIcon } from '../assets/usdt-icon.svg';
 import { ReactComponent as WalletIcon } from '../assets/wallet-icon.svg';
 import { ReactComponent as QuotesIcon } from '../assets/quotes-icon.svg';
+import AccountList from '../../../components/AccountList';
 
 export default function SecondStep() {
   const [updatesIn, setUpdatesIn] = useState(60);
@@ -22,13 +23,29 @@ export default function SecondStep() {
 
   return (
     <div className="transaction-details">
+      <AccountList showMainWallet={false} simplifiedView />
+
+      <div className="transaction-info">
+        <p>You're buying</p>
+        <div>
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '16px', width: '100%' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <p style={{ fontSize: '20px', fontWeight: '700' }}>Premium Passport</p>
+              <p style={{ fontSize: '14px', fontWeight: '400', color: '#989899', width: 'auto !important' }}>365 days</p>
+            </div>
+            <span>Expirantion date: 15/01/2026</span>
+          </div>
+        </div>
+      </div>
+
       <div className="transaction-info">
         <p>Paying with</p>
-        <div>
+        <div className='simple-content'>
           <UsdtIcon />
           <p>USDT</p>
         </div>
       </div>
+
       <div className="transaction-info">
         <p>Wallet</p>
         <div>
@@ -39,6 +56,7 @@ export default function SecondStep() {
           </div>
         </div>
       </div>
+
       <div className="summary">
         <div className="summary-heading">
           <p>Summary</p>
@@ -47,16 +65,20 @@ export default function SecondStep() {
             <p>Quote updates in {updatesIn}s</p>
           </div>
         </div>
+
         <div className="summary-table">
           <div>
             <p>Guardian NFT</p>
             <p>1250 USDT</p>
           </div>
+
           <div>
             <p>GAS Fee</p>
             <p>10 ETH</p>
           </div>
+
           <Separator />
+
           <div>
             <p>Total</p>
             <p>1250 USDT + 10 ETH</p>

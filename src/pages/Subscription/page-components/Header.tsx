87b97c78447053a1060f30e80ee038e6a5e517b5
caiolima-stage <caio.lima@stage.consulting>
2025-04-08 00:23:38 -0300

    chore: back button on loading subscription

--- src/pages/Subscription/page-components/Header.tsx
+++ src/pages/Subscription/page-components/Header.tsx
@@ -4,14 +4,18 @@ import { Step } from '../../../types/global-types';
 export default function Header({ step }: { step: Step }) {
   return (
     <>
-      {(step === 2) && <BackButton to="/wallet" />}
-      {step === 2 && <h1>Confirm your order</h1>}
-      {step === 3 && (
-        <div className="subscription-header">
-          <h1>Transaction in progress</h1>
-          <p>Your order completion time may vary, please wait and we’ll let you know when it’s completed.</p>
+      {(step === 2 || step === 3) && (
+        <div>
+          <BackButton to="/wallet" />
+          {step === 3 && (
+            <div className="subscription-header">
+              <h1>Transaction in progress</h1>
+              <p>Your order completion time may vary, please wait and we’ll let you know when it’s completed.</p>
+            </div>
+          )}
         </div>
       )}
+      {step === 2 && <h1>Confirm your order</h1>}
     </>
   )
 }

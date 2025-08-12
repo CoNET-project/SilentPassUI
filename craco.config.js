d35238ee694e3615ef8f2fbbade3d2f131d25826
YangFei <yff2006_2007@126.com>
2025-08-12 10:04:27 +0800

    rules filter

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,25 @@
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     webpack: {
         alias: {
             '@': path.resolve(__dirname, 'src'), // 让 @ 指向 src 目录
+        },
+        configure: (webpackConfig) => {
+            if (process.env.NODE_ENV === 'production') {
+                webpackConfig.optimization.minimizer = [
+                    new TerserPlugin({
+                        terserOptions: {
+                            compress: {
+                                drop_console: true, // 去除 console
+                                drop_debugger: true // 去除 debugger
+                            }
+                        }
+                    })
+                ];
+            }
+            return webpackConfig;
         }
     }
 };
